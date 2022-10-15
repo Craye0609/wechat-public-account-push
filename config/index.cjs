537ed2c35000d7f07fde46fd2bc387ab4f83d64a
Craye0609 <115786551+Craye0609@users.noreply.github.com>
2022-10-15 16:57:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU16933TgDW2C6qZBXOuIJgPGsMetCw6d1taC0a3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0002',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
