3c07aad013821dc18ae829c7b16d5e49c0b2682a
Craye0609 <115786551+Craye0609@users.noreply.github.com>
2022-10-15 16:58:20 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '牙膏妹妹',
     desc: `
       **{{date.DATA}}**
       
@@ -23,19 +23,6 @@ const TEMPLATE_CONFIG = [
       {{comprehensive_horoscope.DATA}}
       
       ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
-  },
-  {
     id: '0002',
     title: '推送完成提醒',
     desc: `
